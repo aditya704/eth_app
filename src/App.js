931e41eaf1890a1ecbbe8f52ab842e774e4d5454
myselfRJ <r9o9h9i9t91@gmail.com>
2022-07-19 13:27:33 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import { useEffect,useState } from 'react';
+import './App.css';
+import { ethers } from 'ethers';
+import contract from './contracts/NFT11.json';
+
+const contractAddress = "0x95d6c3Da0f7dEbcbFA973017dF41D5DEF428A066";
+const abi = contract.abi;
+
+function App() {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed!");
+      return;
+    } else {
+      console.log("Wallet exists! We're ready to go!")
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Found an account! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  window.ethereum.request({
+    method:'eth_getBalance', 
+    params: [currentAccount]
+}).then(balance => {
+    // Return string value to convert it into int balance
+    console.log(balance,'hhh') 
+      
+    // Yarn add ethers for using ethers utils or
+    // npm install ethers
+    console.log(ethers.utils.formatEther(balance),'fff')
+    // Format the string into main latest balance
+})
+  const mintNftHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        let nftTxn = await nftContract.approve(contractAddress,1)//(1, { value: ethers.utils.parseEther("0.01") });
+
+        console.log("Mining... please wait");
+        await nftTxn.wait();
+console.log(nftTxn)
+        console.log(`Mined, see transaction: http://130.60.24.79:1234/tx/${nftTxn.hash}?network=UZHETH/`);
+
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+        Mint NFT
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
+
+  return (
+    <div className='main-app'>
+      <h1>LuckyCoin11 Minting</h1>
+      <div>
+        {currentAccount ? mintNftButton() : connectWalletButton()}
+      </div>
+    </div>
+  )
+}
+
+export default App;
